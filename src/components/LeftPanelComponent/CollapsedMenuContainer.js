58526a3b4c04b894b1bd6882b714c64dd58e8fc7
Akbar Awais <88664220+AkbarAwais@users.noreply.github.com>
2024-12-04 01:22:51 +0530

    animation improve

--- src/components/LeftPanelComponent/CollapsedMenuContainer.js
+++ src/components/LeftPanelComponent/CollapsedMenuContainer.js
@@ -3,7 +3,7 @@ import { House, Layers2, Youtube, CirclePlay, User, Download } from 'lucide-reac
 
 const CollapsedMenuContainer = () => {
     return (
-        <div className='fixed left-0 top-20 w-20 h-screen border-gray-200 py-4 flex flex-col items-center text-clip'>
+        <div className='fixed left-0 top-18 w-20 h-screen border-gray-200 py-4 flex flex-col items-center text-clip'>
             <div className='flex flex-col items-center space-y-4 w-full text-clip'>
                 <div className='flex flex-col items-center hover:bg-zinc-800 py-2 px-3 rounded-lg cursor-pointer group'>
                     <House className='w-6 h-6' />
