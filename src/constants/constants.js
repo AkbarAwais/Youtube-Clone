5d9e83a9dec1e458d732d2f2af2f182617bd9f94
Akbar Awais <88664220+AkbarAwais@users.noreply.github.com>
2024-12-04 23:27:13 +0530

    env variable

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,1 @@
-export const API_KEY = "AIzaSyCRmg2rYRM8UT52lmxktfWuaHsQiYGJlX8"
-export const LIST_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${API_KEY}`;
+export const LIST_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${process.env.REACT_APP_API_KEY}`;
