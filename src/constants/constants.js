5e317a22264e4174e77d3eaa360b569f87f7fec6
Akbar Awais <88664220+AkbarAwais@users.noreply.github.com>
2024-12-06 19:23:35 +0530

    api

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,1 @@
-export const LIST_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${process.env.API_KEY}`;
+export const LIST_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${process.env.REACT_APP_API_KEY}`;
